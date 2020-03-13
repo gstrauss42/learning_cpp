74e75f94f44fb560d8ab0083374f9bddfd21549b
gstrauss42 <gstrauss42@users.noreply.github.com>
2020-03-13 12:11:46 +0200

    learnt a lot about C++

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main()
+{
+
+    Box box1;
+
+   cout << "Hello World\n"; // prints Hello World
+   return 0;
+}
